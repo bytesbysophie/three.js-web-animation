c5e37e2adb10ec4032ef06b56c68b9aa8185acce
Sophie Krüger <krueger.sophie@googlemail.com>
2021-08-29 17:41:30 +0200

    Add background box and adjustments in color and lightning

--- 3d-scene-with-lightning/main.js
+++ 3d-scene-with-lightning/main.js
@@ -1,9 +1,7 @@
 function init(){ 
-    const color1 = "Turquoise"
-    const color2 = "purple"
-    const backgroundColor = "#191b1c"
+    const color2 = "#841b9e"
+    const color1 = "#007690"
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(backgroundColor)
 
     // Create the cammera 
     const camera = new THREE.PerspectiveCamera(
@@ -20,36 +18,49 @@ function init(){
 
     // Create a 3d torus
     const geometry = new THREE.TorusGeometry(2, 1, 50, 200);
+    const sceneGeometry = new THREE.BoxGeometry( 500, 500, 1000);
 
     // Create a color material 
-    const material = new THREE.MeshPhongMaterial( {color: color2, shininess: 10} );
+    const material = new THREE.MeshPhongMaterial( {color: color2, shininess: 30} );
+    const sceneMaterial = new THREE.MeshPhongMaterial( {color: color1} );
+    sceneMaterial.side = THREE.BackSide; 
 
     // Create torus by combining its geometry with material and add it to the scene
     const torus = new THREE.Mesh( geometry, material );
     torus.castShadow = true; //default is false
-
     scene.add( torus );
 
+    const sceneBox = new THREE.Mesh(sceneGeometry, sceneMaterial);
+    sceneBox.receiveShadow = true; //default
+    scene.add( sceneBox );
+
     // Create light and add it to the scene
-    const lightFront = new THREE.DirectionalLight( "white", 0.5 );
-    lightFront.position.set(0, 50, 300)
+    const lightFront = new THREE.DirectionalLight( "white", 0.3);
+    lightFront.position.set(0, 0, 2)
     scene.add( lightFront );
 
-    const lightTop = new THREE.DirectionalLight("white", 0.5 );
-    lightTop.position.set(0, 400, 40)
+    const lightTop = new THREE.DirectionalLight(color1, 1);
+    lightTop.position.set(-50, 100, 0)
     scene.add( lightTop );
 
-    const lightAmbient = new THREE.AmbientLight(color1)
+    const lightBottom = new THREE.DirectionalLight(color1, 0.7);
+    lightBottom.position.set(50, -40, 100)
+    scene.add( lightBottom );
+
+    const lightAmbient = new THREE.AmbientLight("white")
     scene.add( lightAmbient );
 
     // Adjust camera position to make the object visable
-    camera.position.z = 7;
+    camera.position.z = 10;
+    torus.rotation.y = 10;
+
 
     const animate = () => {
 
         // Change x and y position of the camera to create the rotation effect
-        torus.rotation.y += 0.01;
-    
+        // torus.rotation.y += 0.02;
+        torus.rotation.x += 0.03;
+
         // Render the scene according to the camera settings
         renderer.render(scene, camera)
     
